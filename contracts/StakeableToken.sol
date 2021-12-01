d2a24701c100135ff79dbc25dde8ecf5c2a6c816
0xGOKU <03-chances.yogurts@icloud.com>
2021-12-01 17:41:31 +0200

    refactoring tests, contracts | fix staking & claim

--- contracts/StakeableToken.sol
+++ contracts/StakeableToken.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "hardhat/console.sol";
 
 abstract contract StakeableToken is ERC20, Ownable {
     using SafeMath for uint256;
